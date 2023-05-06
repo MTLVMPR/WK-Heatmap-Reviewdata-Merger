a58c9452e86fd6821a5d88339fea97df2acf0401
Aleksi Viitanen <n1vial00@students.oamk.fi>
2023-05-06 20:45:35 +0300

    Added a copy button

--- Heatmap Review Merger/Script/script.js
+++ Heatmap Review Merger/Script/script.js
@@ -1,22 +1,40 @@
 function superFunc() {
-    const combinedArray = Array.from(new Set(listA.concat(listB).map(JSON.stringify)), JSON.parse);
-    combinedArray.sort((a, b) => {
-        const timestampA = parseInt(a[0]);
-        const timestampB = parseInt(b[0]);
-        if (timestampA < timestampB) {
-          return -1;
-        }
-        if (timestampA > timestampB) {
-          return 1;
-        }
-        return 0;
-    });
+  const combinedArray = Array.from(new Set(listA.concat(listB).map(JSON.stringify)), JSON.parse);
+  combinedArray.sort((a, b) => {
+    const timestampA = parseInt(a[0]);
+    const timestampB = parseInt(b[0]);
+    if (timestampA < timestampB) {
+      return -1;
+    }
+    if (timestampA > timestampB) {
+      return 1;
+    }
+    return 0;
+  });
 
-    const str = JSON.stringify(combinedArray)
-      const pre = document.createElement("pre");
-      pre.textContent = str;
-      document.getElementById("outputter").appendChild(pre);
+  const str = JSON.stringify(combinedArray);
+  const pre = document.createElement("pre");
+  pre.textContent = str;
 
+  // create copy button
+  const copyBtn = document.createElement("button");
+  copyBtn.textContent = "Copy";
+  copyBtn.onclick = () => {
+    const textToCopy = `review_cache.insert(${str.trim()})`;
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        console.log("Text copied to clipboard");
+      })
+      .catch((error) => {
+        console.error("Error copying text to clipboard:", error);
+      });
+  };
+
+  // insert copy button and outputter div
+  const outputter = document.getElementById("outputter");
+  outputter.innerHTML = "";
+  outputter.appendChild(copyBtn);
+  outputter.appendChild(pre);
 }
 
 // Paste your data here in place of the []
@@ -24,4 +42,3 @@ function superFunc() {
 
 let listA = [];
 let listB = [];
-
