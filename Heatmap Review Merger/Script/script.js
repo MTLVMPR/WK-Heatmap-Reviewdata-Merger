4cdc8bff8f27b942c834ce9657537896db7adb95
n1vial00 <n1vial00@students.oamk.fi>
2023-05-15 18:12:01 +0300

    Now removes all the duplicates and handles strings

--- Heatmap Review Merger/Script/script.js
+++ Heatmap Review Merger/Script/script.js
@@ -1,5 +1,22 @@
 function superFunc() {
-  const combinedArray = Array.from(new Set(listA.concat(listB).map(JSON.stringify)), JSON.parse);
+  let listAB;
+  if (typeof listA === 'string') {
+    listAB = JSON.parse(listA);
+  } else {
+    listAB = listA;
+  }
+  let listBB;
+  if (typeof listB === 'string') {
+    listBB = JSON.parse(listB);
+  } else {
+    listBB = listB;
+  }
+  // Combines arrays
+  const combinedArray = Array.from(
+    new Set([...listAB, ...listBB].map(JSON.stringify)),
+    JSON.parse
+  );
+  // Sorting
   combinedArray.sort((a, b) => {
     const timestampA = parseInt(a[0]);
     const timestampB = parseInt(b[0]);
@@ -20,8 +37,11 @@ function superFunc() {
   const copyBtn = document.createElement("button");
   copyBtn.textContent = "Copy";
   copyBtn.onclick = () => {
-    const textToCopy = `review_cache.insert(JSON.parse(${JSON.stringify(str.trim())}))`;
-    navigator.clipboard.writeText(textToCopy)
+    const textToCopy = `review_cache.insert(JSON.parse(${JSON.stringify(
+      str.trim()
+    )}))`;
+    navigator.clipboard
+      .writeText(textToCopy)
       .then(() => {
         console.log("Text copied to clipboard");
       })
@@ -37,9 +57,9 @@ function superFunc() {
   outputter.appendChild(pre);
 }
 
-
 // Paste your data here in place of the []
 // If you have a second list, paste that to the listB
-
+// No need to remove "" since the code does it for you
+ 
 let listA = [];
 let listB = [];
